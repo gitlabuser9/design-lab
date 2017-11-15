7c98ee6eb763ec634b109a142fe9e25cafbf0f37
Crayfish <crayfish@gmail.com>
2017-11-16 00:58:51 +0200

    some minor changes

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import appModule from './app';
 
 import {RegSystemComponent} from "./components/reg-system.component";
+
+
+appModule.component('regList', new RegSystemComponent());
