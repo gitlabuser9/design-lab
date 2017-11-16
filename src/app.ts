2c039a15b34b69df4fcfe1e61ee6374052e73c27
Crayfish <crayfish@gmail.com>
2017-11-16 12:05:17 +0200

    clup

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
 import * as angular from 'angular';
-import * as ngMaterial from 'angular-material';
-const appModule = angular.module('regSystemApp', [ngMaterial]);
+const appModule = angular.module('regSystemApp', []);
 export default appModule;
--- src/components/reg-system.controller.ts
+++ src/components/reg-system.controller.ts
@@ -1,10 +1,4 @@
 import { Lecture } from "./reg.system.model";
-import { Component } from '@angular/core'; 
-
-@Component({
-    selector: 'tabs-overview-example',
-    templateUrl: 'reg-system.template.html',
-})
 
 export class RegSystemController implements ng.IComponentController {
 }
