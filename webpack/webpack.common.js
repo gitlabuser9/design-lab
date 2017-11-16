81444dfad6201ca673742cb36ee5f373d9f68d77
Crayfish <crayfish@gmail.com>
2017-11-16 20:15:09 +0200

    =[

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -15,7 +15,7 @@ module.exports = {
         rules: [
             { test: /\.ts$/, use: 'awesome-typescript-loader' },
             { test: /\.js$/, use: ['ng-annotate-loader', 'babel-loader'], exclude: /node_modules/ },
-            { test: /\.jade$/, use: 'jade-loader'},
+            { test: /\.jade$/, use: ['jade-loader', 'jade-html-loader']},
             { test: /\.html$/, use: 'html-loader'},
             { test: /\.(ttf|eot|svg|woff|woff(2)?)$/, use: 'file-loader?name=fonts/[name].[ext]' },
             { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader' },
