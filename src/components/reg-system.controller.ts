6fa9e49645a8aa46f9ac194188f968b854fee81c
Crayfish <crayfish@gmail.com>
2017-11-18 04:02:18 +0200

    =-[]

--- src/components/reg-system.controller.ts
+++ src/components/reg-system.controller.ts
@@ -1,5 +1,12 @@
 import { Lecture } from "./reg.system.model";
 
 export class RegSystemController implements ng.IComponentController {
+    lectureList: Lecture[]; 
     
+    constructor() {
+        let lectureList = [{ course: 'test', studentCount: 20, registered: 10 }, { course: 'test', studentCount: 20, registered: 10 }, { course: 'test', studentCount: 20, registered: 10}];
+        this.lectureList = lectureList;
+    }
+
+
 }
