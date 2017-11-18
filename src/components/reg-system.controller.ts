f68e28f7471f17c64e2791d339dd8ad2c8611341
Crayfish <crayfish@gmail.com>
2017-11-18 20:18:06 +0200

    321321

--- src/components/reg-system.controller.ts
+++ src/components/reg-system.controller.ts
@@ -4,7 +4,7 @@ export class RegSystemController implements ng.IComponentController {
     lectureList: Lecture[]; 
     
     constructor() {
-        let lectureList = [{ course: 'test', studentCount: 20, registered: 10 }, { course: 'test', studentCount: 20, registered: 10 }, { course: 'test', studentCount: 20, registered: 10}];
+        let lectureList = [{ course: 'test', studentCount: 200, registered: 100 }, { course: 'secon', studentCount: 30, registered: 15 }, { course: 'trd', studentCount: 20, registered: 15}];
         this.lectureList = lectureList;
     }
 
