94dc95d861667f9e8751108428bbc0517b7d2497
Crayfish <crayfish@gmail.com>
2017-12-17 17:42:20 +0200

    9

--- src/components/reg-system.controller.ts
+++ src/components/reg-system.controller.ts
@@ -1,20 +1,22 @@
 import { Lecture } from "./reg.system.model";
 
 export class RegSystemController implements ng.IComponentController {
-    lectureList: Lecture[]; 
-    
+    lectureList: Lecture[] = [
+        { course: 'MIP', studentCount: 40, registered: 30 },
+        { course: 'LAM', studentCount: 30, registered: 15 },
+        { course: 'Datu ieguve', studentCount: 20, registered: 15 }
+    ]; 
+    headings: string[] = ['Course name', 'Student count', 'Registered students'];
+
     constructor() {
-        let lectureList = [
-                            { course: 'predmet', studentCount: 200, registered: 100},
-                            { course: 'polezniy predmet', studentCount: 30, registered: 15},
-                            { course: 'predmet', studentCount: 20, registered: 15}
-                        ];
-        this.lectureList = lectureList;
+        this.lectureList;
+        this.headings;
     }
 
     addRegistration(lecture: Lecture) {
-        if (lecture.registered < lecture.studentCount)
+        if (lecture.registered < lecture.studentCount) {
             lecture.registered++;
+        }
     }
 
 }
